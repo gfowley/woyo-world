45ccd23526661ba971682cc12afafbb73a9d4169
Gerard Fowley <gerard.fowley@iqeo.net>
2014-04-12 15:28:03 -0400

    character

---
+++ lib/woyo/character.rb
@@ -0,0 +1,43 @@
+
+module Woyo
+
+class Character
+
+  @@attributes = [ :name, :description ]
+
+  @@attributes.each do |attr|
+    self.class_eval("
+      def #{attr}= arg
+        @attributes[:#{attr}] = @#{attr} = arg
+      end
+      def #{attr}(arg=nil)
+        if arg.nil?
+          @#{attr}
+        else
+          self.#{attr} = arg
+        end
+      end
+    ")
+  end
+
+  def self.attributes
+    @@attributes
+  end
+
+  attr_reader :id, :attributes
+  attr_accessor :_test
+
+  def initialize id, &block
+    @id = id.to_s.downcase.to_sym
+    @attributes = {}
+    evaluate &block
+  end
+
+  def evaluate &block
+    (block.arity < 1 ? (instance_eval &block) : block.call(self)) if block_given?
+    self
+  end
+
+end
+
+end
---
+++ spec/woyo/character_spec.rb
@@ -0,0 +1,111 @@
+require 'woyo/character'
+
+describe Woyo::Character do
+
+  context '#new' do
+
+    it 'initializes with symbol id' do
+      expect { Woyo::Character.new :bilbo }.to_not raise_error
+    end
+
+    it 'creates id' do
+      Woyo::Character.new(:my_id).id.should eq :my_id
+    end
+
+    it 'converts id to lowercase' do
+      Woyo::Character.new('MY_id').id.should eq :my_id
+      Woyo::Character.new(:MY_id ).id.should eq :my_id
+    end
+
+    # it 'accepts named parameter world:' do
+    #   expect { Woyo::Character.new(:my_id, world: Woyo::World.new) }.to_not raise_error
+    # end
+
+    it 'accepts a block with arity 0' do
+      result = ''
+      Woyo::Character.new( :bilbo ) { result = 'ok' }
+      result.should eq 'ok'
+    end
+
+    it 'instance evals block with arity 0' do
+      Woyo::Character.new( :bilbo ) { self.class.should == Woyo::Character }
+    end
+
+    it 'accepts a block with arity 1' do
+      result = ''
+      Woyo::Character.new( :bilbo ) { |loc| result = 'ok' }
+      result.should eq 'ok'
+    end
+
+    it 'passes self to block with arity 1' do
+      Woyo::Character.new( :bilbo ) { |loc| loc.should be_instance_of Woyo::Character }
+    end
+
+  end
+
+  context '#evaluate' do
+
+    it 'instance evals block with arity 0' do
+      bilbo = Woyo::Character.new( :bilbo )
+      other = bilbo.evaluate { self.should == bilbo }
+      other.should be bilbo
+    end
+
+    it 'passes self to block with arity 1' do
+      bilbo = Woyo::Character.new( :bilbo )
+      other = bilbo.evaluate { |loc| loc.should be bilbo }
+      other.should be bilbo
+    end
+
+  end
+
+  context 'attributes' do
+
+    it 'names can be listed for class' do
+      attrs = Woyo::Character.attributes
+      attrs.should be_instance_of Array
+      attrs.all? { |a| a.is_a? Symbol }.should be_true
+    end                       
+
+    it 'names and values can be listed for instance' do
+      bilbo = Woyo::Character.new :bilbo
+      bilbo.attributes.should be_instance_of Hash
+    end
+
+    it 'can be written with =' do
+      bilbo = Woyo::Character.new :bilbo
+      Woyo::Character.attributes.each do |attr|
+        eval "bilbo.#{attr} = '#{attr}'.upcase"
+      end
+      bilbo.attributes.count.should eq Woyo::Character.attributes.count
+      bilbo.attributes.each do |name,value|
+        value.should eq name.to_s.upcase
+      end
+    end
+
+    it 'can be written without =' do
+      bilbo = Woyo::Character.new :bilbo
+      Woyo::Character.attributes.each do |attr|
+        eval "bilbo.#{attr} '#{attr}'.upcase"
+      end
+      bilbo.attributes.count.should eq Woyo::Character.attributes.count
+      bilbo.attributes.each do |name,value|
+        value.should eq name.to_s.upcase
+      end
+    end
+
+    it 'can be read' do
+      bilbo = Woyo::Character.new :bilbo
+      Woyo::Character.attributes.each do |attr|
+        eval "bilbo.#{attr} '#{attr}'.upcase"
+      end
+      bilbo.attributes.count.should eq Woyo::Character.attributes.count
+      bilbo.attributes.each do |name,value|
+        eval("bilbo.#{name}").should eq value
+      end
+    end
+
+  end
+
+end
+
