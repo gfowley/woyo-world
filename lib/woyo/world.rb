a1010530840ff8fdfa2c1962f7a3a5b440a35e42
Gerard Fowley <gerard.fowley@iqeo.net>
2014-04-16 18:14:52 -0400

    world contains character

--- lib/woyo/world.rb
+++ lib/woyo/world.rb
@@ -8,30 +8,13 @@ module Woyo
 class World
 
   include DSL
-  contains :location
+  contains :location, :character
 
-  attr_reader :items, :characters
+  attr_reader :items
 
   def initialize &block
     @items = {}
-    @characters = {}
     evaluate &block
-  end
-
-  def character char_or_id, &block
-    character = char_or_id.kind_of?( Character ) ? char_or_id : nil
-    id = character ? character.id : char_or_id
-    known = @characters[id] ? true : false
-    case
-    when  character &&  known &&  block_given? then character.evaluate &block
-    when  character &&  known && !block_given? then character
-    when  character && !known &&  block_given? then @characters[id] = character.evaluate &block
-    when  character && !known && !block_given? then @characters[id] = character
-    when !character &&  known &&  block_given? then @characters[id].evaluate &block
-    when !character &&  known && !block_given? then @characters[id]
-    when !character && !known &&  block_given? then @characters[id] = Character.new id, context: self, &block
-    when !character && !known && !block_given? then @characters[id] = Character.new id, context: self
-    end
   end
 
 end
