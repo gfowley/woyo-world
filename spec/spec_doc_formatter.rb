1a65254c4b6a853deda250348db41eace240f5ae
Gerard Fowley <gerard.fowley@iqeo.net>
2014-06-11 18:46:46 -0400

    specdoc outputs json

--- spec/spec_doc_formatter.rb
+++ spec/spec_doc_formatter.rb
@@ -13,32 +13,30 @@ class SpecDocFormatter < RSpec::Core::Formatters::BaseFormatter
 
   def initialize(output)
     super
-    @group_level = 0
   end
   
   def start(start_notification)
     @specdoc_root = {}
     @specdoc_group = @specdoc_root
     @specdoc_parents = []
-    output.puts "### start          :"
   end
 
   def stop(examples_notification)
-    output.puts "### stop           :"
+    output.puts JSON.pretty_generate @specdoc_root
   end
 
   def example_group_started(group_notification)
-    group_notification.group.init_specdoc
-    this_group_specdoc = group_notification.group.metadata[:specdoc][group_notification.group] 
-    this_group_specdoc[:examples] = []
-    @specdoc_group[group_notification.group.description] = this_group_specdoc
+    this_group = group_notification.group
+    this_group.init_specdoc
+    this_group_specdoc = this_group.metadata[:specdoc][this_group] 
+    @specdoc_group[this_group.description] = this_group_specdoc
     @specdoc_parents << @specdoc_group
     @specdoc_group = this_group_specdoc
-    output.puts "### group start    :#{indent}#{group_notification.group.description} #=> #{@specdoc_group}"
+    @specdoc_group[:heading] ||= this_group.description.capitalize
+    @specdoc_group[:examples] = []
   end
 
   def example_group_finished(group_notification)
-    output.puts "### group finish   :#{indent}#{group_notification.group.description} #=> #{@specdoc_group}"
     @specdoc_group = @specdoc_parents.pop
   end
 
@@ -55,8 +53,10 @@ class SpecDocFormatter < RSpec::Core::Formatters::BaseFormatter
   end
 
   def collect_example example_notification, result
-    @specdoc_group[:examples] << example_notification.example.metadata[:specdoc]
-    output.puts "### example #{result} :#{indent}:#{example_notification.example.description} #=> #{@specdoc_group[:examples].last}"
+    this_example = example_notification.example
+    this_specdoc = this_example.metadata[:specdoc]
+    this_specdoc[:heading] ||= this_example.description.capitalize
+    @specdoc_group[:examples] << this_specdoc 
   end
 
   def indent
